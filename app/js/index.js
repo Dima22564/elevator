f2486627e8e61d64f0a80d860f7bc7b313f97c0a
Dmitriy <drakon-sayron@mail.ru>
2019-10-11 02:07:58 +0500

    10.10.19

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
-  
+  $('.js-burger-icon').click(function(e){
+    $('.js-burger-menu').slideToggle();
+  });
+
+  // Slider under  header
+  $('.js-slider-header').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    prevArrow: $('.js-btn-prev'),
+    nextArrow: $('.js-btn-next'),
+  })
 });
